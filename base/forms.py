b5c734c4352209e69fdeb86add6039fd74ea7ddc
bimbozled <abenbow1@charlotte.edu>
2025-04-23 16:35:13 -0400

    To-do fix WIP

--- base/forms.py
+++ base/forms.py
@@ -1,12 +1,35 @@
 from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, User
 
 
 class MyUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    name = forms.CharField(max_length=200, required=True)
+    
     class Meta:
         model = User
-        fields = ['name', 'username', 'email', 'password1', 'password2']
+        fields = ['name', 'email', 'password1', 'password2']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields['name'].required = True
+        
+        # Make error messages more user-friendly
+        self.fields['password1'].help_text = "Your password must contain at least 8 characters."
+        self.fields['password2'].help_text = "Enter the same password as before, for verification."
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.username = self.cleaned_data['email']  # Set username to email
+        user.name = self.cleaned_data['name']
+        
+        if commit:
+            user.save()
+        return user
 
 
 class RoomForm(ModelForm):
